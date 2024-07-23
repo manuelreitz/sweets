93358b1211ab56a6db318921985a2f5d3b841d74
manuelreitz <manuelreitz@users.noreply.github.com>
2024-07-23 16:15:21 +0200

    old layout to start fresh

--- colors.js
+++ colors.js
@@ -1,23 +1,9 @@
-// const categoryColors = {
-//     "Background": "#FFFFFF",
-//     "Raw": "#7B4EB0",          
-//     "Cooked": "#F57F7F",       
-//     "Partly Refined": "#6E9B55", 
-//     "Other Refined": "#E59755", 
-//     "Refined Sugar": "#A03372",  
-//     "Sugar Alcohol": "#6F6379",  
-//     "Synthetic Sugar": "#C79C99",
-//     "Synthetic Sugar Alcohol": "#4D7FA0"
-// };
-
 const categoryColors = {
+
     "Background": "#e84310",
-    "Naturstoff roh": "#54b23b",          
-    "Naturstoff verarbeitet": "#f0e73b",       
-    "Zucker": "#e6689a", 
+    "Naturstoff roh": "#f78c2d", 
+    "Zucker": "#e6689a",          
+    "Naturstoff verarbeitet": "#b573e0",       
     "Zuckeralkohol": "#42b4dd", 
-    "synthetischer Süßstoff": "#ea2d5a",  
-    "Sugar Alcohol": "#b573e0",  
-    "Synthetic Sugar": "#6dbfa6",
-    "Synthetic Sugar Alcohol": "#f78c2d"
+    "synthetischer Süßstoff": "#ea2d5a"
 };
